46cc0f2e3ed97a9d8c3a41a3e23e9d651733d12b
L-u-a-u <lua-u1@proton.me>
2025-05-24 17:52:46 -0700

    Create mainstuffidkihateyourfilesyslol.lua

---
+++ mainstuffidkihateyourfilesyslol.lua
@@ -0,0 +1,6 @@
+
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/loadownercmdss.lua"))()
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/betterchatdtcsyss.lua"))()
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/akactivee.lua"))()
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/loadplayertagss.lua"))()
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/antihttpspy.lua"))()
